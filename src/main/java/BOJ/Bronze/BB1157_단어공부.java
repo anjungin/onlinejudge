3a2c0b082d6a6726c16a0bca054ef967b12375fe
ttingin <ttingin5745@gmail.com>
2026-01-05 20:43:51 +0900

    단어공부 완성

--- src/main/java/BOJ/Bronze/BB1157_단어공부.java
+++ src/main/java/BOJ/Bronze/BB1157_단어공부.java
@@ -2,27 +2,41 @@ package BOJ.Bronze;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class BB1157_단어공부 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String n = br.readLine().toUpperCase();
-        int max=0;
-        String aphabet = "";
+        Integer[] arr = new Integer[26];
+        for (int i = 0; i < 26; i++) {
+            arr[i] = 0;
+        }
 
-        while (n.length()!=0) {
+        while (!n.isEmpty()) {
             char first = n.charAt(0);
             int distance = n.length() - n.replaceAll(String.valueOf(first),"").length();
-            if (max==distance) {
-                System.out.println("?");
-                return;
-            } else if (max<distance) {
-                max=distance;
-                aphabet= String.valueOf(first);
-            }
+            arr[first - 65] = distance;
             n = n.replaceAll(String.valueOf(first),"");
         }
 
-        System.out.println(aphabet);
+        int max=0;
+        int secondMax=0;
+        int str = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (max<=arr[i]) {
+                secondMax=max;
+                max=arr[i];
+                str = i;
+            }
+        }
+
+        if (max==secondMax) {
+            System.out.println("?");
+            return;
+        }
+
+        System.out.print((char)(str+65));
     }
 }
